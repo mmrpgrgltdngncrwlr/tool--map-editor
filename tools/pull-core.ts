f1b05251eaa6866d31af5ffc37d36ca6f6aeee3c
ericchase <github@eric.addymail.com>
2025-08-15 13:30:43 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/pull-core.ts
+++ tools/pull-core.ts
@@ -2,12 +2,13 @@ import { Step_Dev_Project_Sync_Core } from './core-dev/step/Step_Dev_Project_Syn
 import { Builder } from './core/Builder.js';
 import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 
-//
 // This script pulls core lib files from template project.
+
+const template_path = 'C:/Code/Base/JavaScript-TypeScript/Templates/Self-Hosted-Web-App';
 
 Builder.SetStartUpSteps(
   Step_Bun_Run({ cmd: ['bun', 'install'], showlogs: false }),
-  Step_Dev_Project_Sync_Core({ from_path: 'C:/Code/Base/JavaScript-TypeScript/Templates/Self-Hosted-Web-App', to_path: './' }),
+  Step_Dev_Project_Sync_Core({ from_path: template_path, to_path: '.' }),
   //
 );
 
--- tools/pull-lib.ts
+++ tools/pull-lib.ts
@@ -1,17 +1,25 @@
 import { NODE_PATH } from '../src/lib/ericchase/NodePlatform.js';
 import { Builder } from './core/Builder.js';
+import { Step_Async } from './core/step/Step_Async.js';
 import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 import { Step_FS_Mirror_Directory } from './core/step/Step_FS_Mirror_Directory.js';
 
 // This script pulls template lib files from template project.
 
+const template_path = 'C:/Code/Base/JavaScript-TypeScript/Templates/Self-Hosted-Web-App';
+const lib_folders: string[] = ['lib-self-hosted-web-app'];
+
 Builder.SetStartUpSteps(
   Step_Bun_Run({ cmd: ['bun', 'install'], showlogs: false }),
-  Step_FS_Mirror_Directory({
-    from_path: NODE_PATH.join('C:/Code/Base/JavaScript-TypeScript/Templates/Self-Hosted-Web-App', 'tools/lib-self-hosted-web-app'),
-    to_path: NODE_PATH.join(Builder.Dir.Tools, 'lib-self-hosted-web-app'),
-    include_patterns: ['**/*'],
-  }),
+  Step_Async(
+    lib_folders.map((dir: string) =>
+      Step_FS_Mirror_Directory({
+        from_path: NODE_PATH.join(template_path, 'tools/' + dir),
+        to_path: NODE_PATH.join(Builder.Dir.Tools, dir),
+        include_patterns: ['**/*'],
+      }),
+    ),
+  ),
   //
 );
 
