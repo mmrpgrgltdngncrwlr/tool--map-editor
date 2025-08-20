58af4aaf5ed5e65fba727b7e17b65ebd942b5014
Hussain Abidi <hussainabidi2009@gmail.com>
2025-08-20 22:23:04 +0500

    Simple tile editor

--- out/authenticated/index.module.js
+++ out/authenticated/index.module.js
@@ -1,34 +1,34 @@
 // src/lib/ericchase/Core_Console_Error.ts
 function Core_Console_Error(...items) {
-  console['error'](...items);
+  console["error"](...items);
 }
 
 // src/lib/ericchase/WebPlatform_DOM_ReadyState_Callback.ts
 async function Async_WebPlatform_DOM_ReadyState_Callback(config) {
   async function DOMContentLoaded() {
-    removeEventListener('DOMContentLoaded', DOMContentLoaded);
+    removeEventListener("DOMContentLoaded", DOMContentLoaded);
     await config.DOMContentLoaded?.();
   }
   async function load() {
-    removeEventListener('load', load);
+    removeEventListener("load", load);
     await config.load?.();
   }
   switch (document.readyState) {
-    case 'loading':
+    case "loading":
       if (config.DOMContentLoaded !== undefined) {
-        addEventListener('DOMContentLoaded', DOMContentLoaded);
+        addEventListener("DOMContentLoaded", DOMContentLoaded);
       }
       if (config.load !== undefined) {
-        addEventListener('load', load);
+        addEventListener("load", load);
       }
       break;
-    case 'interactive':
+    case "interactive":
       await config.DOMContentLoaded?.();
       if (config.load !== undefined) {
-        addEventListener('load', load);
+        addEventListener("load", load);
       }
       break;
-    case 'complete':
+    case "complete":
       await config.DOMContentLoaded?.();
       await config.load?.();
       break;
@@ -87,8 +87,8 @@ function WebPlatform_Node_Reference_Class(node) {
 }
 
 // src/lib/ClientMutex.ts
-var DATABASE_NAME = 'ClientMutex';
-var STORE_NAME = 'lock';
+var DATABASE_NAME = "ClientMutex";
+var STORE_NAME = "lock";
 async function Async_AcquireClientMutex() {
   const database = await new Promise((resolve, reject) => {
     const open_request = indexedDB.open(DATABASE_NAME, 1);
@@ -107,13 +107,13 @@ async function Async_AcquireClientMutex() {
     };
   });
   return new Promise((resolve, reject) => {
-    const transaction = database.transaction(STORE_NAME, 'readwrite');
+    const transaction = database.transaction(STORE_NAME, "readwrite");
     transaction.objectStore(STORE_NAME).put(Date.now(), STORE_NAME);
     transaction.oncomplete = () => {
       return resolve({
         release: () => {
           return new Promise((resolve2, reject2) => {
-            const transaction2 = database.transaction(STORE_NAME, 'readwrite');
+            const transaction2 = database.transaction(STORE_NAME, "readwrite");
             transaction2.objectStore(STORE_NAME).delete(STORE_NAME);
             transaction2.oncomplete = () => {
               return resolve2();
@@ -122,7 +122,7 @@ async function Async_AcquireClientMutex() {
               return reject2(transaction2.error ?? new Error(`Failed to acquire the "${DATABASE_NAME}" lock.`));
             };
           });
-        },
+        }
       });
     };
     transaction.onerror = () => {
@@ -139,32 +139,61 @@ async function Async_MutexFetch(request_fn, response_cb) {
 }
 async function Async_UnpairAllClientsFromServer() {
   return await fetch(`${window.location.origin}/api/authentication/unpair`, {
-    method: 'POST',
+    method: "POST"
   });
 }
 
 // src/authenticated/index.module.ts
-var button_unpair = WebPlatform_Node_Reference_Class(document.getElementById('unpair')).as(HTMLButtonElement);
+var button_unpair = WebPlatform_Node_Reference_Class(document.getElementById("unpair")).as(HTMLButtonElement);
 await Async_WebPlatform_DOM_ReadyState_Callback({
   async load() {
-    button_unpair.addEventListener('click', async_unpair);
-  },
+    button_unpair.addEventListener("click", async_unpair);
+  }
 });
 async function async_unpair() {
   try {
-    await Async_MutexFetch(
-      () => Async_UnpairAllClientsFromServer(),
-      async (response) => {
-        switch (response.status) {
-          case 200:
-            window.location.href = '/';
-            break;
-          default:
-            Core_Console_Error(await response.text());
-        }
-      },
-    );
+    await Async_MutexFetch(() => Async_UnpairAllClientsFromServer(), async (response) => {
+      switch (response.status) {
+        case 200:
+          window.location.href = "/";
+          break;
+        default:
+          Core_Console_Error(await response.text());
+      }
+    });
   } catch (error) {
     Core_Console_Error(error);
   }
 }
+var mapWidth = 8;
+var mapHeight = 8;
+var tileSize = 32;
+var canvas = document.getElementById("editorCanvas");
+canvas.width = mapWidth * tileSize;
+canvas.height = mapHeight * tileSize;
+var ctx = canvas.getContext("2d");
+
+class Tile {
+}
+var grid = new Map;
+for (let x = 0;x < mapWidth; x++) {
+  for (let y = 0;y < mapHeight; y++) {
+    grid.set(`${x * tileSize},${y * tileSize}`, null);
+  }
+}
+var dirtImage = new Image;
+dirtImage.src = "./assets/4d5bbuxo.bmp";
+canvas.onclick = (ev) => {
+  let snappedX = Math.floor(ev.offsetX / tileSize) * tileSize;
+  let snappedY = Math.floor(ev.offsetY / tileSize) * tileSize;
+  const posKey = `${snappedX},${snappedY}`;
+  let cell = grid.get(posKey);
+  if (!cell) {
+    grid.set(posKey, new Tile);
+    ctx.drawImage(dirtImage, snappedX, snappedY, tileSize, tileSize);
+  } else {
+    grid.set(posKey, null);
+    ctx.clearRect(snappedX, snappedY, tileSize, tileSize);
+  }
+  console.log(snappedX, snappedY);
+};
--- src/authenticated/index.module.ts
+++ src/authenticated/index.module.ts
@@ -29,3 +29,40 @@ async function async_unpair() {
     Core_Console_Error(error);
   }
 }
+
+const mapWidth = 8; // tiles
+const mapHeight = 8; // tiles
+const tileSize = 32; // pixels
+
+const canvas = document.getElementById('editorCanvas') as HTMLCanvasElement;
+canvas.width = mapWidth * tileSize;
+canvas.height = mapHeight * tileSize;
+
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+class Tile {}
+
+const grid = new Map<string, Tile | null>();
+for (let x = 0; x < mapWidth; x++) {
+  for (let y = 0; y < mapHeight; y++) {
+    grid.set(`${x * tileSize},${y * tileSize}`, null);
+  }
+}
+
+let dirtImage = new Image();
+dirtImage.src = './assets/4d5bbuxo.bmp';
+
+canvas.onclick = (ev) => {
+  let snappedX = Math.floor(ev.offsetX / tileSize) * tileSize;
+  let snappedY = Math.floor(ev.offsetY / tileSize) * tileSize;
+  const posKey = `${snappedX},${snappedY}`;
+  let cell = grid.get(posKey);
+  if (!cell) {
+    grid.set(posKey, new Tile());
+    ctx.drawImage(dirtImage, snappedX, snappedY, tileSize, tileSize);
+  } else {
+    grid.set(posKey, null);
+    ctx.clearRect(snappedX, snappedY, tileSize, tileSize);
+  }
+  console.log(snappedX, snappedY);
+};
